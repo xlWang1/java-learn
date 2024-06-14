60deb2a607cf64b315b4c8638dafd097a4afe5fe
xlWang <1448039778@qq.com>
2024-06-14 10:51:37 +0800

    ..

---
+++ chapter12/src/TestCollectionContains.java
@@ -0,0 +1,84 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class TestCollectionContains {
+
+    /*
+     * boolean contains(Object obj)：判断当前集合中是否存在一个与obj对象equals返回true的元素
+     * boolean containsAll(Collection coll)：判断coll集合中的元素是否在当前集合中都存在。即coll集合是否是当前集合的“子集”
+     * boolean equals(Object obj)：判断当前集合与obj是否相等
+     *
+     * */
+    @Test
+    public void test01() {
+        Collection coll = new ArrayList();
+        System.out.println("coll在添加元素之前，isEmpty = " + coll.isEmpty());
+        coll.add("小李广");
+        coll.add("扫地僧");
+        coll.add("石破天");
+        coll.add("佛地魔");
+        System.out.println("coll的元素个数" + coll.size());
+        System.out.println("coll在添加元素之后，isEmpty = " + coll.isEmpty());
+    }
+
+    @Test
+    public void test02() {
+        Collection coll = new ArrayList();
+        coll.add("小李广");
+        coll.add("扫地僧宋");
+        coll.add("石破天康");
+        coll.add("佛地魔红");
+        System.out.println("coll = " + coll);
+        System.out.println("coll是否包含“小李广” = " + coll.contains("小李广"));
+        System.out.println("coll是否包含“宋红康” = " + coll.contains("宋红康"));
+
+        Collection other = new ArrayList();
+        other.add("小李广");
+        other.add("扫地僧");
+        other.add("尚硅谷");
+        System.out.println("other = " + other);
+
+        System.out.println("coll.containsAll(other) = " + coll.containsAll(other));
+    }
+
+    @Test
+    public void test03(){
+        Collection c1 = new ArrayList();
+        c1.add(1);
+        c1.add(2);
+        System.out.println("c1集合元素的个数：" + c1.size());//2
+        System.out.println("c1 = " + c1);
+
+        Collection c2 = new ArrayList();
+        c2.add(1);
+        c2.add(2);
+        System.out.println("c2集合元素的个数：" + c2.size());//2
+        System.out.println("c2 = " + c2);
+
+        Collection other = new ArrayList();
+        other.add(1);
+        other.add(2);
+        other.add(3);
+        System.out.println("other集合元素的个数：" + other.size());//3
+        System.out.println("other = " + other);
+        System.out.println();
+
+        c1.addAll(other);
+        System.out.println("c1集合元素的个数：" + c1.size());//5
+        System.out.println("c1.addAll(other) = " + c1);
+        System.out.println("c1.contains(other) = " + c1.contains(other));
+        System.out.println("c1.containsAll(other) = " + c1.containsAll(other));
+        System.out.println();
+
+        c2.add(other);
+        System.out.println("c2集合元素的个数：" + c2.size());
+        System.out.println("c2.add(other) = " + c2);
+        System.out.println("c2.contains(other) = " + c2.contains(other));
+        System.out.println("c2.containsAll(other) = " + c2.containsAll(other));
+    }
+
+}
+
+
